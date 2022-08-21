01b2fc9010d4a0b4ce6d5c3af4b2132b0ad5e512
xhasu <ing.xhasu@gmail.com>
2022-08-21 10:02:35 -0500

    add config project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["pages/**/*.{js,jsx,mdx}", "components/**/*.{js,jsx,mdx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        century: ["CenturyGothic", "sans-serif"],
+        acuminpro: ["AcuminProWide", "sans-serif"],
+        myriadpro: ["MyriadPro", "sans-serif"],
+      },
+      colors: {
+        primary: "#fcd700",
+      },
+      letterSpacing: {
+        extrawide: "0.6rem",
+      },
+    },
+  },
+  plugins: [],
+};
