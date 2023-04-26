323f3f2097456e369c44f51cffc97879b1a53fd2
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 18:23:32 -0300

    Update sticker-manufacturer.jsx

--- pages/visual-graphics/sticker-manufacturer.jsx
+++ pages/visual-graphics/sticker-manufacturer.jsx
@@ -44,18 +44,15 @@ const StickerManufacturerPage = () => {
           </div>
     <Product.Grid>
          <Product.GridItem>
-        <Product.Description>
-              <p>
-                        Bring life to any room or office with our custom wall decals and graphics. Vinyl
-        can be applied to walls with any texture like, concrete, Sheetrock, wood,
-        aluminum ect. We use 3M vinyl for the highest quality and can last long periods
-        of time.
-              </p>
-    </Product.Description>
-         </Product.GridItem>
-
-        </Product.Grid>
-               <Product.GridItem>
+            <Product.Description>
+                <p>
+                  Bring life to any room or office with our custom wall decals and graphics. Vinyl
+                  can be applied to walls with any texture like, concrete, Sheetrock, wood,
+                  aluminum ect. We use 3M vinyl for the highest quality and can last long periods
+                  of time.
+                </p>
+            </Product.Description>
+                   <Product.GridItem>
             <img
               src="/images/content/large-format-printing-mural-3m.jpg"
               alt="Large format printing for store front with 3m vinyl graphics. Any design can be printed and installed on windows and walls"
@@ -65,6 +62,10 @@ const StickerManufacturerPage = () => {
               height="450"
             />
           </Product.GridItem>
+    
+         </Product.GridItem>
+
+       </Product.Grid>
             
 
         </Product.Head>
