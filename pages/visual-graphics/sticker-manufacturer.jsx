c9e8d65da7117e79522538a66c8c20ad4d49b656
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 18:24:55 -0300

    Update sticker-manufacturer.jsx

--- pages/visual-graphics/sticker-manufacturer.jsx
+++ pages/visual-graphics/sticker-manufacturer.jsx
@@ -51,8 +51,11 @@ const StickerManufacturerPage = () => {
                   aluminum ect. We use 3M vinyl for the highest quality and can last long periods
                   of time.
                 </p>
+
             </Product.Description>
-                   <Product.GridItem>
+    
+         </Product.GridItem>
+        <Product.GridItem>
             <img
               src="/images/content/large-format-printing-mural-3m.jpg"
               alt="Large format printing for store front with 3m vinyl graphics. Any design can be printed and installed on windows and walls"
@@ -62,9 +65,6 @@ const StickerManufacturerPage = () => {
               height="450"
             />
           </Product.GridItem>
-    
-         </Product.GridItem>
-
        </Product.Grid>
             
 
