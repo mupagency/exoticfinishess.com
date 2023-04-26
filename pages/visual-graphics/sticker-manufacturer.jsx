fbda7b25435b081561ad0a9c7e50c348c2c07691
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 18:29:14 -0300

    Update sticker-manufacturer.jsx

--- pages/visual-graphics/sticker-manufacturer.jsx
+++ pages/visual-graphics/sticker-manufacturer.jsx
@@ -4,72 +4,38 @@ import Product from "components/shared/product";
 const StickerManufacturerPage = () => {
   return (
     <GraphicLayout>
-      <div className="container">
+       <div className="container">
         <Product.Head>
           <div>
             <Product.Title>
-            Wall <br /> Graphics
+              Window Frost <br /> Privacy Film
             </Product.Title>
           </div>
           <div>
             <Product.Description>
               <p>
-                Vinyl can be applied to walls or windows to improve your brand visibility, for that reason we use 3M
-                vinyl for the highest quality and can last long periods of time. <br />
+                Increase the privacy of your home or business with window frost. This film creates a classic frosted
+                design to your space without blocking light. Use it on standard windows to keep others from seeing
+                inside your home or add accents to your windows. It&apos;s an easy way to provide accents and define
+                unique spaces.
               </p>
             </Product.Description>
-     
-           <Product.GridItem>
-            <img
-              src="/images/content/wall-graphics-3m-vinyl.jpg"
-              alt="Large format printing for store front with 3m vinyl graphics. Any design can be printed and installed on windows and walls"
-              loading="lazy"
-              className="max-w-full"
-              width="600"
-              height="450"
-            />
-          </Product.GridItem>
+          </div>
+        </Product.Head>
         <Product.Grid>
           <Product.GridItem>
-            <Product.Subtitle>Wall Graphics</Product.Subtitle>
-            <Product.Parragraph>
-              Bring life to any room or office with our custom wall decals and graphics. Vinyl
-              can be applied to walls with any texture like, concrete, Sheetrock, wood,
-              aluminum ect. We use 3M vinyl for the highest quality and can last long periods
-              of time
-            </Product.Parragraph>
-          </Product.GridItem>
-
-        </Product.Grid>
-          </div>
-    <Product.Grid>
-         <Product.GridItem>
-            <Product.Description>
-                <p>
-                  Bring life to any room or office with our custom wall decals and graphics. Vinyl
-                  can be applied to walls with any texture like, concrete, Sheetrock, wood,
-                  aluminum ect. We use 3M vinyl for the highest quality and can last long periods
-                  of time.
-                </p>
-
-            </Product.Description>
-    
-         </Product.GridItem>
-        <Product.GridItem>
             <img
-              src="/images/content/large-format-printing-mural-3m.jpg"
-              alt="Large format printing for store front with 3m vinyl graphics. Any design can be printed and installed on windows and walls"
-              loading="lazy"
-              className="max-w-full"
-              width="600"
-              height="450"
+              src="/images/content/window-frost-privacy-film.jpg"
+              alt="Window frost privacy film installed on windows in an office space"
             />
           </Product.GridItem>
-       </Product.Grid>
-            
-
-        </Product.Head>
-
+          <Product.GridItem>
+            <img
+              src="/images/content/partial-window-frost-privacy-film.jpg"
+              alt="Partial window frost privacy film installed on glass for office with custom design"
+            />
+          </Product.GridItem>
+        </Product.Grid>
       </div>
     </GraphicLayout>
   );
