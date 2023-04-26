e2fa4a1947bcdfe01ab78693665705dfc44b702b
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 18:31:24 -0300

    Update sticker-manufacturer.jsx

--- pages/visual-graphics/sticker-manufacturer.jsx
+++ pages/visual-graphics/sticker-manufacturer.jsx
@@ -8,16 +8,14 @@ const StickerManufacturerPage = () => {
         <Product.Head>
           <div>
             <Product.Title>
-              Window Frost <br /> Privacy Film
+              Sticker <br /> Manufacturer
             </Product.Title>
           </div>
           <div>
             <Product.Description>
               <p>
-                Increase the privacy of your home or business with window frost. This film creates a classic frosted
-                design to your space without blocking light. Use it on standard windows to keep others from seeing
-                inside your home or add accents to your windows. It&apos;s an easy way to provide accents and define
-                unique spaces.
+               As many as you need, we are able to print stickers from any artwork,
+               picture or logo. Fast turn around times and high quality vinyl. Waterproof, STICKER UV resistant and don’t fade
               </p>
             </Product.Description>
           </div>
@@ -25,13 +23,13 @@ const StickerManufacturerPage = () => {
         <Product.Grid>
           <Product.GridItem>
             <img
-              src="/images/content/window-frost-privacy-film.jpg"
+              src="/images/content/stickers-manufacturer-decals-3m.jpg"
               alt="Window frost privacy film installed on windows in an office space"
             />
           </Product.GridItem>
           <Product.GridItem>
             <img
-              src="/images/content/partial-window-frost-privacy-film.jpg"
+              src="/images/content/stickers-manufacturer-decals.jpg"
               alt="Partial window frost privacy film installed on glass for office with custom design"
             />
           </Product.GridItem>
