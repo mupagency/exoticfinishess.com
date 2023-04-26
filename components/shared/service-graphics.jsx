665aaf2c240055a936e33653fcdeab9bd358a675
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 01:33:20 -0300

    Update service-graphics.jsx

--- components/shared/service-graphics.jsx
+++ components/shared/service-graphics.jsx
@@ -27,24 +27,6 @@ const ServiceGraphics = () => {
             </Item>
           </a>
         </Link>
-        <Link href="/visual-graphics/commercial-vehicle-wraps/">
-          <a>
-            <Item title="Commercial Vehicle Wraps">
-              <picture>
-                <source
-                  srcSet="/images/navigation/mobile/commercial-car-wrap-design-mercedes-benz-van.jpg"
-                  media="(max-width: 768px)"
-                />
-                <img
-                  src="/images/navigation/commercial-car-wrap-design-mercedes-benz-van.jpg"
-                  alt="Commercial car wrap for Zurikate using 3m IJ180 vinyl and custom design"
-                  width="348"
-                  height="499"
-                />
-              </picture>
-            </Item>
-          </a>
-        </Link>
         <Link href="/visual-graphics/sticker-manufacturer/">
           <a>
             <Item title="sticker Manufacturer">
@@ -78,24 +60,7 @@ const ServiceGraphics = () => {
             </Item>
           </a>
         </Link>
-        <Link href="/visual-graphics/solar-window-film/">
-          <a>
-            <Item title="Solar Window Film">
-              <picture>
-                <source
-                  srcSet="/images/navigation/mobile/xpel-vision-clear-view-plus-thumb.jpg"
-                  media="(max-width: 768px)"
-                />
-                <img
-                  src="/images/navigation/xpel-vision-clear-view-plus-thumb.jpg"
-                  alt="Xpel vision clear view plus solar window film installed in a house. Window tint with maximum technology to keep the heat away"
-                  width="348"
-                  height="499"
-                />
-              </picture>
-            </Item>
-          </a>
-        </Link>
+      
       </NavWrap>
     </div>
   );
