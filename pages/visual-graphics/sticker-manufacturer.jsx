7b90ba7b8e21f285d796db48b91a0f923c4fd7d8
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 02:50:47 -0300

    Update sticker-manufacturer.jsx

--- pages/visual-graphics/sticker-manufacturer.jsx
+++ pages/visual-graphics/sticker-manufacturer.jsx
@@ -18,7 +18,7 @@ const StickerManufacturerPage = () => {
             </Product.Description>
            <Product.GridItem>
             <img
-              src="/images/content/window-graphics-large-format-printing-3m.jpg"
+              src="/images/content/wall-graphics-3m-vinyl.jpg"
               alt="Large format printing for store front with 3m vinyl graphics. Any design can be printed and installed on windows and walls"
               loading="lazy"
               className="max-w-full"
@@ -35,15 +35,26 @@ const StickerManufacturerPage = () => {
               aluminum ect. We use 3M vinyl for the highest quality and can last long periods
               of time
             </Product.Parragraph>
-            <Product.Parragraph>
-              We offer in house design, print and installation and service all United States. We use the best of vinyl
-              graphics with our exotic laminations between glossy, matte, sparkle, textures and more.
-            </Product.Parragraph>
           </Product.GridItem>
 
         </Product.Grid>
           </div>
-
+<Product.Description>
+              <p>
+                Vinyl can be applied to walls or windows to improve your brand visibility, for that reason we use 3M
+                vinyl for the highest quality and can last long periods of time.
+              </p>
+            </Product.Description>
+           <Product.GridItem>
+            <img
+              src="/images/content/wall-graphics-3m-vinyl.jpg"
+              alt="Large format printing for store front with 3m vinyl graphics. Any design can be printed and installed on windows and walls"
+              loading="lazy"
+              className="max-w-full"
+              width="600"
+              height="450"
+            />
+          </Product.GridItem>
         </Product.Head>
 
       </div>
