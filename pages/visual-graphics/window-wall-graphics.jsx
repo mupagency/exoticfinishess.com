8fbdc2345d65da528da5d4df608829bdf1f77198
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 02:06:56 -0300

    Update window-wall-graphics.jsx

--- pages/visual-graphics/window-wall-graphics.jsx
+++ pages/visual-graphics/window-wall-graphics.jsx
@@ -18,18 +18,19 @@ const WindowWallGraphicsPage = () => {
                 vinyl for the highest quality and can last long periods of time.
               </p>
             </Product.Description>
-
-          </div>
-                  <Product.GridItem>
+           <Product.GridItem>
             <img
               src="/images/content/window-graphics-large-format-printing-3m.jpg"
               alt="Large format printing for store front with 3m vinyl graphics. Any design can be printed and installed on windows and walls"
               loading="lazy"
               className="max-w-full"
-              width="733"
-              height="550"
+              width="600"
+              height="450"
             />
           </Product.GridItem>
+
+          </div>
+
         </Product.Head>
 
         <Product.Grid>
