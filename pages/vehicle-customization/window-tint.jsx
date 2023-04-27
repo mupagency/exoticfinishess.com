72464a7be42d4ca5c47f4d02f0aa15bcaa2b950e
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 22:21:59 -0300

    Update window-tint.jsx

--- pages/vehicle-customization/window-tint.jsx
+++ pages/vehicle-customization/window-tint.jsx
@@ -1,10 +1,5 @@
 import VehicleLayout from "components/layouts/vehicle";
 import Product from "components/shared/product";
-export const NavWrap = ({ children }) => {
-  return (
-      <div className="container">{children}</div>
-  );
-};
 
 const WindowTintPage = () => {
   return (
@@ -41,23 +36,7 @@ const WindowTintPage = () => {
         <div className="mb-20 product-media">
           <img src="/images/content/prime-xr.png" alt="Prime XR automotive window film" className="mx-auto" />
         </div>
-    export const NavItem = ({ children, title = "", type = "" }) => {
-  return (
-    <div className="relative h-[190px] lg:h-full">
-      <div className="relative z-0 w-full h-full">{children}</div>
-      <div className="absolute inset-0 bg-stone-900 bg-opacity-60 hover:bg-opacity-0 transition-colors z-0"></div>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full pointer-events-none">
-        <h2 className="mb-2 text-white uppercase text-sm md:text-xl lg:text-2xl font-bold text-center px-4">{title}</h2>
-        <div className="flex justify-center text-white">
-          {type == "xpel" && (
-            <img src="/images/navigation/xpel.png" alt="Exotic Finishess Xpel" width="85" height="22" />
-          )}
-          {type == "3m" && <img src="/images/navigation/3m.png" alt="Exotic Finishess 3M" width="51" height="27" />}
-        </div>
-      </div>
-    </div>
-  );
-};
+
       </div>
     </VehicleLayout>
   );
