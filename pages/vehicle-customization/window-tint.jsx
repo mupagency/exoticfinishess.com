6f2225e6c305cee3aa00a51f70b60b1285c6f7ea
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 22:08:18 -0300

    Update window-tint.jsx

--- pages/vehicle-customization/window-tint.jsx
+++ pages/vehicle-customization/window-tint.jsx
@@ -1,20 +1,15 @@
 import VehicleLayout from "components/layouts/vehicle";
 import Product from "components/shared/product";
-export const NavWrap = ({ children }) => {
-  return (
-    <nav className="flex justify-center py-8 px-4">
-      <div className="flex flex-col md:flex-row rounded-3xl overflow-hidden">{children}</div>
-    </nav>
-  );
-};
 
-export const NavItem = ({ children, title = "", type = "" }) => {
+const WindowTintPage = () => {
   return (
-    <div className="relative h-[190px] lg:h-full">
-      <div className="relative z-0 w-full h-full">{children}</div>
+    <VehicleLayout>
+      <div className="container">
+            <div className="relative h-[190px] lg:h-full">
+      <div className="relative z-0 w-full h-full">AAAAAA</div>
       <div className="absolute inset-0 bg-stone-900 bg-opacity-60 hover:bg-opacity-0 transition-colors z-0"></div>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full pointer-events-none">
-        <h2 className="mb-2 text-white uppercase text-sm md:text-xl lg:text-2xl font-bold text-center px-4">{title}</h2>
+        <h2 className="mb-2 text-white uppercase text-sm md:text-xl lg:text-2xl font-bold text-center px-4">AAAAA</h2>
         <div className="flex justify-center text-white">
           {type == "xpel" && (
             <img src="/images/navigation/xpel.png" alt="Exotic Finishess Xpel" width="85" height="22" />
@@ -23,5 +18,9 @@ export const NavItem = ({ children, title = "", type = "" }) => {
         </div>
       </div>
     </div>
+      </div>
+    </VehicleLayout>
   );
 };
+
+export default WindowTintPage;
