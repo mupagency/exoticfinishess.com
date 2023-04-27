297f59b9d89d6486a5e3ec431dbd8cb7e5ab8934
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 23:23:37 -0300

    Update window-tint.jsx

--- pages/vehicle-customization/window-tint.jsx
+++ pages/vehicle-customization/window-tint.jsx
@@ -7,54 +7,43 @@ import { NavWrap, NavItem as Item } from "components/shared/custom-nav";
 const WindowTintPage = () => {
   return (
     <VehicleLayout>
-      <div className="container">
+<div className="container">
         <Product.Head>
           <div>
-            <Product.Title>
-              Window <br /> Tint
-              <span className="inline-block align-middle">
-                <img src="/images/xpel.png" alt="ExoticFinishes Xpel" width="103" height="27" />
-              </span>
-            </Product.Title>
+            <Product.Title>Powder Coating Painting</Product.Title>
           </div>
           <div>
             <Product.Description>
-              <Product.Subtitle>NANO-CERAMIC WINDOW TINT</Product.Subtitle>
               <p>
-                If you&apos;re looking for maximum performance & protection from your window tint, look no further than
-                nano-ceramic window film like PRIME XR PLUS. Ceramic tint is designed to block out the most infrared
-                heat possible, while reflecting harmful UV rays to keep you safe. No matter the shade, ceramic window
-                tint delivers results you expect
+                It is the best protection you can provide to the surfaces of your vehicle, it is a thermoplastic
+                transparent film, designed to protect painted surfaces from damage caused by abrasion, corrosion,
+                chemicals or minor damage by road stones.
+              </p>
+              <p>
+                Protect your investment from damage such as stones, scratches, friction with another surface, this is
+                possible with the installation of Paint Protection Film (PPF).
               </p>
             </Product.Description>
           </div>
         </Product.Head>
-        <div className="mb-8 product-media">
-          <img
-            src="/images/content/nano-ceramic-window-tint-xpel.jpg"
-            alt="Window tint for a 2022 Tesla Model Y with XR plus nano-ceramic film"
-          />
-        </div>
-        <h2 className="font-acuminpro text-3xl uppercase text-center mb-12 px-4">We Carry:</h2>
-        <div className="mb-20 product-media">
-          <img src="/images/content/prime-xr.png" alt="Prime XR automotive window film" className="mx-auto" />
-        </div>
-            <Link href="/visual-graphics/window-frost-privacy-film/">
-          <a>
-            <Item title="Window Frost Privacy Film">
-              <picture>
-                <source srcSet="/images/navigation/mobile/window-frost-privacy-film.jpg" media="(max-width: 768px)" />
-                <img
-                  src="/images/navigation/window-frost-privacy-film.jpg"
-                  alt="Partial window frost privacy film installed on glass for office with custom design"
-                  width="348"
-                  height="499"
-                />
-              </picture>
-            </Item>
-          </a>
-        </Link>
-
+        <Product.Grid>
+          <Product.GridItem>
+            <Product.Subtitle>Powder coating wheel paint</Product.Subtitle>
+            <Product.Parragraph>
+              Powder coating is an electrically-bonded powder that when sprayed, sticks to the metal of the wheels via a
+              process called &quot;electro-static attraction.&quot; As the atomized paint molecules are
+              &quot;misted&quot; onto the wheel, they create a coating that can be baked into the surface via extreme
+              heat with a kiln, or oven.
+            </Product.Parragraph>
+          </Product.GridItem>
+          <Product.GridItem>
+            <img
+              src="/images/content/powder-coating-wheel-paint-mercedes-benz.jpg"
+              alt="Powder coat wheels on a Mercedes Benz AMG 53 in gloss black with red calipers"
+              className="max-w-full"
+            />
+          </Product.GridItem>
+        </Product.Grid>
       </div>
     </VehicleLayout>
   );
