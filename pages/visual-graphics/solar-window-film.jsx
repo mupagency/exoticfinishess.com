e168ca22109fd520c48ee24f1984e7b3217b8347
Agusmazz <85418369+Agusmazz@users.noreply.github.com>
2023-04-26 21:22:40 -0300

    Update solar-window-film.jsx

--- pages/visual-graphics/solar-window-film.jsx
+++ pages/visual-graphics/solar-window-film.jsx
@@ -7,31 +7,99 @@ const SolarWindowFilmPage = () => {
       <div className="container">
         <Product.Head>
           <div>
-            <Product.Title> Solar Window Film</Product.Title>
+            <Product.Title>
+              Paint Protection <br /> Film
+              <span className="inline-block align-middle">
+                <img src="/images/xpel.png" alt="ExoticFinishes Xpel" width="103" height="27" />
+              </span>
+            </Product.Title>
           </div>
           <div>
             <Product.Description>
               <p>
-                If you&apos;re ready to take control of temperatures in your home or office and add next-level privacy
-                with UV protection, look no further than VISION Solar Window Film or mostly known as window tint.{" "}
+                It is the best protection you can provide to the surfaces of your vehicle, it is a thermoplastic
+                transparent film, designed to protect painted surfaces from damage caused by abrasion, corrosion,
+                chemicals or minor damage by road stones.
+              </p>
+              <p>
+                Protect your investment from damage such as stones, scratches, friction with another surface, this is
+                possible with the installation of Paint Protection Film (PPF).
               </p>
             </Product.Description>
           </div>
         </Product.Head>
         <Product.Grid>
           <Product.GridItem>
-            <Product.Subtitle>Clear view</Product.Subtitle>
-            <Product.Parragraph>Includes: Clear View Ceramic, Clear View Alloy, And Clear View Plus</Product.Parragraph>
+            <Product.Subtitle>Self-Healing</Product.Subtitle>
             <Product.Parragraph>
-              CLEAR VIEW films are designed to filter in visible light while cutting Infrared heat and UV rays. These
-              features optimize visibility from both sides of the glass.
+              When exposed to heat, the enhanced clear coat properties eliminate fine scratches and swirl marks over
+              time.
+            </Product.Parragraph>
+            <div className="mb-8"></div>
+            <Product.Subtitle>Sleek Satin Finish</Product.Subtitle>
+            <Product.Parragraph>
+              With an unique satin finish, STEALTH helps match most factory flat paint jobs and virtually disappears on
+              most surfaces. With a finish that&apos;s uniformly smooth, you can add STEALTH to your matte or gloss
+              paint job for a unique satin finish.
+            </Product.Parragraph>
+            <div className="mb-8"></div>
+            <Product.Subtitle>Easy Maintenance</Product.Subtitle>
+            <Product.Parragraph>
+              Flat finishes are notoriously difficult to maintain and even harder to repair. STEALTH enables you to
+              easily wash and dry your car without damaging its sheen.
             </Product.Parragraph>
           </Product.GridItem>
           <Product.GridItem>
             <img
-              src="/images/content/xpel-vision-clear-view-plus-thumb.jpg"
-              alt="Xpel vision clear view plus solar window film installed in a house. Window tint with maximum technology to keep the heat away"
+              src="/images/content/stealth-ppf-xpel-bmw-m240i.jpg"
+              alt="2022 BMW 240i with XPEL stealth paint protection film. Black out wheels from Zurikate"
               className="max-w-full"
+            />
+          </Product.GridItem>
+        </Product.Grid>
+        <Product.Grid>
+          <Product.GridItemImg>
+            <img
+              src="/images/content/paint-protection-film-ultimate-fusion-gloss-with-ceramic-xpel.jpg"
+              alt="Process of ultimate fusion XPEL in gloss paint protection film being installed on a 2022 Ford F250 Harley Davidson"
+              className="max-w-full"
+            />
+          </Product.GridItemImg>
+          <Product.GridItem>
+            <Product.Subtitle>Next level dual-purpose protection</Product.Subtitle>
+            <Product.Parragraph>
+              ULTIMATE FUSION is an optically clear, high gloss, self-healing film that protects vehicles from rock
+              chips, scuffs, and light scratches. Developed with a hydrophobic top-coat, the film&apos;s naturally slick
+              surface helps repel water, making protected surfaces easier to wash and stay clean.
+            </Product.Parragraph>
+            <Product.Parragraph>Hydrophobic Properties: Repels water, dirt, and road grime.</Product.Parragraph>
+            <Product.Parragraph>
+              Provides Protection From The Elements: Formulated to protect against damaging oxidation, insect acids,
+              corrosion, and contaminants.
+            </Product.Parragraph>
+            <Product.Parragraph>
+              Improves Surface Clarity: Increases color depth, leaving PPF with a smooth and slick finish.
+            </Product.Parragraph>
+          </Product.GridItem>
+        </Product.Grid>
+        <div className="mb-8 product-media">
+          <img
+            src="/images/content/lamborghini-paint-protection-film-ultimate-fusion-gloss-with-ceramic-xpel.jpg"
+            alt="Lamborghini with XPEL ultimate fusion plus in gloss with ceramic paint protection film"
+          />
+        </div>
+        <h2 className="font-acuminpro text-3xl uppercase text-center mb-8 px-4">PPF XPEL SMOKED HEAD LIGHT</h2>
+        <Product.Grid>
+          <Product.GridItem>
+            <img
+              src="/images/content/ppf-xpel-smoked-head-lights-tesla-y-1.jpg"
+              alt="Process of XPEL XR plus on headlights for 2022 Tesla Model Y"
+            />
+          </Product.GridItem>
+          <Product.GridItem>
+            <img
+              src="/images/content/ppf-xpel-smoked-head-lights-tesla-y-2.jpg"
+              alt="Smoked headlights with XPEL XR plus on 2022 Tesla Model Y. Stealth paint protection film xpel"
             />
           </Product.GridItem>
         </Product.Grid>
