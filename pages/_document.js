42a2f210bcbf4513617e50acef4b531102131cdd
xhasu <ing.xhasu@gmail.com>
2024-12-06 21:13:31 -0500

    Add alternate link tags for language localization in document head

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { locales } from "providers/language-provider";
 
 export default function Document() {
   return (
@@ -6,6 +7,10 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="/css/fonts.css" />
         <link rel="stylesheet" href="/css/app.css" />
+        {locales.map((locale) => (
+          <link key={locale} rel="alternate" hrefLang={locale} href={`https://www.exoticfinishess.com/`} />
+        ))}
+        <link rel="alternate" hrefLang="x-default" href={`https://www.exoticfinishess.com/`} />
       </Head>
       <body className="font-century min-w-[320px]">
         <noscript
